d2bb2d4e92b2fc1f21a0d04618b3ef7efffe3e7e
Hugohugan123 <46204949+Hugohugan123@users.noreply.github.com>
2024-12-11 15:39:52 +0100

    Update WebGLtest.js

--- WebGLtest/WebGLtest.js
+++ WebGLtest/WebGLtest.js
@@ -1,15 +1,14 @@
 main();
 
-//
-// start here
-//
+
+
 function main() {
-  const canvas = document.querySelector("#glcanvas");
+    const canvas = document.getElementById("glcanvas");
   // Initialize the GL context
   const gl = canvas.getContext("webgl");
 
   // Only continue if WebGL is available and working
-  if (gl === null) {
+  if (!gl) {
     alert(
       "Unable to initialize WebGL. Your browser or machine may not support it.",
     );
@@ -17,7 +16,7 @@ function main() {
   }
 
   // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(0.0, 0.0, 1.0, 1.0);
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
